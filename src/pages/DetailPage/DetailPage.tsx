6de29c074a8dff6b4abb63fb244d4a5a128cf39d
chaengs <cathyshim222@naver.com>
2022-08-17 17:43:39 +0900

    ♻️ #7 데이터 지정을 구조분해할당 방법으로 수정

--- src/pages/DetailPage/DetailPage.tsx
+++ src/pages/DetailPage/DetailPage.tsx
@@ -2,15 +2,8 @@ import React from 'react'
 import { useLocation } from 'react-router-dom'
 import { BookInfoType } from '../../types/bookType'
 export default function DetailPage() {
-	const location = useLocation()
-
-	const state = location.state as BookInfoType
-	const title = state.title
-	const thumbnail = state.thumbnail
-	const authors = state.authors
-	const contents = state.contents
-	const datetime = state.datetime
-	const publisher = state.publisher
+	const { state } = useLocation()
+	const { thumbnail, title, authors, contents, datetime, publisher } = state as BookInfoType
 
 	return (
 		<section>
